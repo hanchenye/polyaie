06a6a2aaaa72e9d500f1b8d597dda4ef830d4917
Hanchen Ye <hanchenye@gmail.com>
2021-11-18 20:29:17 -0600

    [PrintDataflow] Remove non-call users from the graph printing

--- lib/Transforms/PrintDataflow.cpp
+++ lib/Transforms/PrintDataflow.cpp
@@ -12,64 +12,49 @@ using namespace mlir;
 using namespace polyaie;
 
 namespace llvm {
-
-// Specialize GraphTraits to treat Block as a graph of Operations as nodes and
+// Specialize GraphTraits to treat ModuleOp as a graph of CallOps as nodes and
 // uses as edges.
 template <> struct GraphTraits<ModuleOp> {
   using GraphType = ModuleOp;
   using NodeRef = Operation *;
 
-  using ChildIteratorType = Operation::user_iterator;
-  static ChildIteratorType child_begin(NodeRef n) { return n->user_begin(); }
-  static ChildIteratorType child_end(NodeRef n) { return n->user_end(); }
+  static bool isCall(Operation *op) { return isa<CallOp>(op); }
+  using ChildIteratorType =
+      llvm::filter_iterator<Operation::user_iterator, decltype(&isCall)>;
+  static ChildIteratorType child_begin(NodeRef n) {
+    return {n->user_begin(), n->user_end(), &isCall};
+  }
+  static ChildIteratorType child_end(NodeRef n) {
+    return {n->user_end(), n->user_end(), &isCall};
+  }
 
-  // Operation's destructor is private so use Operation* instead and use
-  // mapped iterator.
-  static Operation *AddressOf(const CallOp &op) { return op; }
-  using nodes_iterator =
-      mapped_iterator<mlir::detail::op_iterator<CallOp, Region::OpIterator>,
-                      decltype(&AddressOf)>;
+  using nodes_iterator = mlir::detail::op_iterator<CallOp, Region::OpIterator>;
   static nodes_iterator nodes_begin(ModuleOp m) {
-    return nodes_iterator(m.getOps<CallOp>().begin(), &AddressOf);
+    return m.getOps<CallOp>().begin();
   }
   static nodes_iterator nodes_end(ModuleOp m) {
-    return nodes_iterator(m.getOps<CallOp>().end(), &AddressOf);
+    return m.getOps<CallOp>().end();
   }
 };
 
 // Specialize DOTGraphTraits to produce more readable output.
 template <> struct DOTGraphTraits<ModuleOp> : public DefaultDOTGraphTraits {
   using DefaultDOTGraphTraits::DefaultDOTGraphTraits;
-  static std::string getNodeLabel(Operation *op, ModuleOp);
+
+  static std::string getNodeLabel(Operation *op, ModuleOp) {
+    // Reuse the print output for the node labels.
+    std::string ostr;
+    raw_string_ostream os(ostr);
+    os << dyn_cast<CallOp>(op).callee().rsplit("_").second;
+    return os.str();
+  }
 };
-
-std::string DOTGraphTraits<ModuleOp>::getNodeLabel(Operation *op, ModuleOp m) {
-  // Reuse the print output for the node labels.
-  std::string ostr;
-  raw_string_ostream os(ostr);
-  // os << op->getName() << " : ";
-
-  // Print resultant types.
-  // llvm::interleaveComma(op->getResultTypes(), os);
-
-  // Print callee.
-  auto callOp = dyn_cast<CallOp>(op);
-  // os << "\ncallee: ";
-  os << callOp.callee().rsplit("_").second;
-
-  return os.str();
-}
-
-} // end namespace llvm
+} // namespace llvm
 
 namespace {
-// PrintDataflow is simple pass to write graph per function.
-// Note: this is a module pass only to avoid interleaving on the same ostream
-// due to multi-threading over functions.
 class PrintDataflow : public polyaie::PrintDataflowBase<PrintDataflow> {
 public:
   PrintDataflow(raw_ostream &os) : os(os) {}
-
   void runOnOperation() override {
     llvm::WriteGraph(os, getOperation(), false, "");
   }
