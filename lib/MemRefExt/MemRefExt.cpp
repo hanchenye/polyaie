fb00eea2f9f1211eac4be440aab414c245cb4f08
Hanchen Ye <hanchenye@gmail.com>
2021-11-19 22:35:32 -0600

    [MemRefExt] Add LoadBufferOp and StoreBufferOp

--- lib/MemRefExt/MemRefExt.cpp
+++ lib/MemRefExt/MemRefExt.cpp
@@ -5,6 +5,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "polyaie/MemRefExt/MemRefExt.h"
+#include "mlir/IR/AffineMap.h"
 #include "mlir/IR/Builders.h"
 
 using namespace mlir;
@@ -19,6 +20,39 @@ void MemRefExtDialect::initialize() {
 }
 
 #include "polyaie/MemRefExt/MemRefExtEnums.cpp.inc"
+
+template <class OpType>
+static LogicalResult verifyLoadStoreBufferOp(OpType op) {
+  auto bufferType = op.buffer().getType().template cast<MemRefType>();
+  auto memoryType = op.memory().getType().template cast<MemRefType>();
+  if (bufferType.getNumElements() > memoryType.getNumElements())
+    return op.emitOpError("buffer size is larger than memory size");
+
+  SmallVector<int64_t, 4> offsets;
+  for (auto attr : op.offsets())
+    offsets.push_back(attr.template cast<IntegerAttr>().getInt());
+
+  SmallVector<int64_t, 4> lengths;
+  for (auto attr : op.lengths())
+    lengths.push_back(attr.template cast<IntegerAttr>().getInt());
+
+  if (bufferType.getRank() == 0) {
+    if (offsets.size() != 1 || lengths.size() != 1 || offsets.front() != 0 ||
+        lengths.front() != 1)
+      return op.emitOpError("illegal offsets or lengths attribute");
+  } else if (bufferType.getShape() != ArrayRef<int64_t>(lengths))
+    return op.emitOpError("illegal lengths attribute");
+
+  return success();
+}
+
+static LogicalResult verify(LoadBufferOp op) {
+  return verifyLoadStoreBufferOp(op);
+}
+
+static LogicalResult verify(StoreBufferOp op) {
+  return verifyLoadStoreBufferOp(op);
+}
 
 #define GET_OP_CLASSES
 #include "polyaie/MemRefExt/MemRefExt.cpp.inc"
