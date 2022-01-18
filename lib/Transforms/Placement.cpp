925751a8f7d2386c339b0670ce6f908b6b2a528c
Hanchen Ye <hanchenye@gmail.com>
2022-01-18 17:22:24 -0600

    [Placement] Add Layout, AIELayout, ShimLayout, and ShimNOCLayout class

--- lib/Transforms/Placement.cpp
+++ lib/Transforms/Placement.cpp
@@ -9,6 +9,226 @@
 
 using namespace mlir;
 using namespace polyaie;
+using namespace dataflow;
+
+using PhysicalLocation = std::pair<unsigned, unsigned>;
+
+//===----------------------------------------------------------------------===//
+// A general-purpose layout class
+//===----------------------------------------------------------------------===//
+
+namespace {
+class Layout {
+public:
+  Layout() = delete;
+  Layout(const Layout &other) : nodes(other.nodes), layout(other.layout) {}
+  Layout(const SmallVector<Operation *, 32> &nodes, unsigned size)
+      : nodes(nodes), layout(size) {
+    assert(size >= nodes.size() && "layout size is not enough");
+    DenseSet<Operation *> nodeSet;
+    for (auto node : nodes)
+      assert(nodeSet.insert(node).second && "duplicate node");
+  }
+  virtual ~Layout() = default;
+
+  /// Get the physical location of a node if it exists in the layout.
+  Optional<PhysicalLocation> getPhysicalLoc(Operation *node) const {
+    if (auto loc = find(node))
+      return logicalToPhysicalLoc(loc.getValue());
+    return llvm::None;
+  }
+
+  /// Get the node placed on the given physical location. If the location is
+  /// empty, return nullptr.
+  Operation *getNode(PhysicalLocation physLoc) const {
+    return layout[physicalToLogicalLoc(physLoc)];
+  }
+
+  /// Check whether all nodes have been placed.
+  LogicalResult checkFullyPlaced() const {
+    // Check whether any node is placed more than once.
+    DenseSet<Operation *> placedNodeSet;
+    for (auto node : llvm::make_filter_range(
+             layout, [](Operation *node) -> bool { return node; }))
+      if (!placedNodeSet.insert(node).second)
+        return failure();
+
+    // Check whether any node is not placed.
+    for (auto node : nodes)
+      if (!placedNodeSet.count(node))
+        return failure();
+
+    return success();
+  }
+
+  /// Erase all placed nodes in the layout.
+  void clear() {
+    for (auto &node : layout)
+      node = nullptr;
+  }
+
+  /// Initialize all nodes to a random logical location.
+  void randomInitialize() {
+    clear();
+    unsigned loc = 0;
+    for (auto node : nodes)
+      layout[loc++] = node;
+    llvm::shuffle(layout.begin(), layout.end(), []() { return std::rand(); });
+  }
+
+  /// Swap a random node into a new random location.
+  void randomSwap(unsigned iteration = 1) {
+    for (unsigned i = 0; i < iteration; ++i) {
+      auto node = nodes[std::rand() % nodes.size()];
+      auto loc = find(node);
+      assert(loc && "node has not been placed");
+
+      auto swapLoc = std::rand() % layout.size();
+      auto swapNode = layout[swapLoc];
+
+      layout[loc.getValue()] = swapNode;
+      layout[swapLoc] = node;
+    }
+  }
+
+protected:
+  /// Find the logical location given a node.
+  /// TODO: We should finally have a DenseMap<Operation *, unsigned> to find the
+  /// logical location to get O(logn) performance. This requires all non-const
+  /// methods to be enhanced.
+  Optional<unsigned> find(Operation *node) const {
+    auto nodePtr = llvm::find(layout, node);
+    if (nodePtr == layout.end())
+      return llvm::None;
+    return nodePtr - layout.begin();
+  }
+
+  /// Return whether a logical location is legal.
+  bool verifyLogicalLoc(unsigned loc) const { return loc < layout.size(); }
+
+  /// Return whether a physical location is legal.
+  virtual bool verifyPhysicalLoc(PhysicalLocation physLoc) const = 0;
+
+  /// Get the physical location given the logical location.
+  virtual PhysicalLocation logicalToPhysicalLoc(unsigned loc) const = 0;
+
+  /// Get the logical location given the physical location.
+  virtual unsigned physicalToLogicalLoc(PhysicalLocation physLoc) const = 0;
+
+private:
+  /// All the nodes in the layout. They may or may not have been placed.
+  const SmallVector<Operation *, 32> &nodes;
+
+  /// Record the node placed on each logical location.
+  SmallVector<Operation *, 32> layout;
+};
+} // namespace
+
+//===----------------------------------------------------------------------===//
+// AIE layout class
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// PhysicalLocation is interpreted as [row, col].
+class AIELayout : public Layout {
+public:
+  AIELayout(const SmallVector<Operation *, 32> &nodes, unsigned rowBegin = 1,
+            unsigned colBegin = 0, unsigned rowNum = 8, unsigned colNum = 50)
+      : Layout(nodes, rowNum * colNum), rowBegin(rowBegin), colBegin(colBegin),
+        rowNum(rowNum), colNum(colNum) {
+    assert(rowBegin > 0 && rowBegin + rowNum <= 8 && colBegin + colNum <= 50 &&
+           "illegal AIE range");
+  }
+
+private:
+  /// Return whether a physical location is legal.
+  bool verifyPhysicalLoc(PhysicalLocation physLoc) const override {
+    return physLoc.first - rowBegin < rowNum &&
+           physLoc.second - colBegin < colNum;
+  }
+
+  /// Get the physical location given the logical location.
+  PhysicalLocation logicalToPhysicalLoc(unsigned loc) const override {
+    assert(verifyLogicalLoc(loc) && "illegal logical location");
+    return {loc / colNum + rowBegin, loc % colNum + colBegin};
+  }
+
+  /// Get the logical location given the physical location.
+  unsigned physicalToLogicalLoc(PhysicalLocation physLoc) const override {
+    assert(verifyPhysicalLoc(physLoc) && "illegal physical location");
+    return (physLoc.first - rowBegin) * colNum + physLoc.second - colBegin;
+  }
+
+  /// The range of AIEs for placement.
+  const unsigned rowBegin, colBegin, rowNum, colNum;
+};
+} // namespace
+
+//===----------------------------------------------------------------------===//
+// Shim layout class
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// PhysicalLocation is interpreted as [col, port].
+/// TODO: Support to apply constraints like AIELayout.
+class ShimLayout : public Layout {
+public:
+  ShimLayout(const SmallVector<Operation *, 32> &nodes,
+             ArrayRef<unsigned> shimCols, unsigned portNum)
+      : Layout(nodes, shimCols.size() * portNum),
+        shimCols(shimCols.begin(), shimCols.end()), portNum(portNum) {}
+
+private:
+  /// Return whether a physical location is legal.
+  bool verifyPhysicalLoc(PhysicalLocation physLoc) const override {
+    return llvm::find(shimCols, physLoc.first) != shimCols.end() &&
+           physLoc.second < portNum;
+  }
+
+  /// Get the physical location given the logical location.
+  PhysicalLocation logicalToPhysicalLoc(unsigned loc) const override {
+    assert(verifyLogicalLoc(loc) && "illegal logical location");
+    return {shimCols[loc / portNum], loc % portNum};
+  }
+
+  /// Get the logical location given the physical location.
+  unsigned physicalToLogicalLoc(PhysicalLocation physLoc) const override {
+    assert(verifyPhysicalLoc(physLoc) && "illegal physical location");
+    return (llvm::find(shimCols, physLoc.first) - shimCols.begin()) * portNum +
+           physLoc.second;
+  }
+
+  /// This array holds the columns that have IO available.
+  const SmallVector<unsigned, 16> shimCols;
+
+  /// The number of ports on each shim tile.
+  const unsigned portNum;
+};
+} // namespace
+
+//===----------------------------------------------------------------------===//
+// ShimNOC and ShimPL layout class
+//===----------------------------------------------------------------------===//
+
+/// There're in total 16 ShimNOCs that can directly access the external DDR.
+/// They are distributed to the 50 shim tiles and each ShimNOC has two input and
+/// two output ports that can be connected to AIE. This array holds the columns
+/// that have ShimNOC available.
+static const unsigned ShimNOC[16] = {2,  3,  6,  7,  10, 11, 18, 19,
+                                     26, 27, 34, 35, 42, 43, 46, 47};
+
+namespace {
+/// Could be used for both input and output ports.
+class ShimNOCLayout : public ShimLayout {
+public:
+  ShimNOCLayout(const SmallVector<Operation *, 32> &nodes)
+      : ShimLayout(nodes, ShimNOC, 2) {}
+};
+} // namespace
+
+//===----------------------------------------------------------------------===//
+// A placer class extensible for multiple algorithms
+//===----------------------------------------------------------------------===//
 
 namespace {
 class Placer {
@@ -58,6 +278,9 @@ private:
   SmallVector<dataflow::ProcessOp, 128> layout;
   /// Record the coordinates(index) of each dataflow::ProcessOp.
   DenseMap<Operation *, unsigned> layoutMap;
+
+  // const unsigned PLInputs[39 * 8] = {};
+  // const unsigned PLOutputs[39 * 6] = {};
 };
 } // namespace
 
